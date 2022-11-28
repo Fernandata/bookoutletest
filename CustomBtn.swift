de8f794d325f56069a655fe07d9d2430c481575a
Luis Fernando Salas Gave <lsalagav@emeal.nttdata.com>
2022-11-27 23:33:27 -0500

    Complete welcome view

---
+++ CustomBtn.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBtn.swift
+//  BookOutlet
+//
+//  Created by Luis Fernando Salas Gave on 27/11/22.
+//
+
+import UIKit
+
+class CustomBtn: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.backgroundColor = UIColor(red: 107/255, green: 105/255, blue: 193/255, alpha: 1).cgColor
+        layer.cornerRadius = 10
+      
+    }
+}
---
+++ CustomBtn1.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBtn1.swift
+//  BookOutlet
+//
+//  Created by Luis Fernando Salas Gave on 27/11/22.
+//
+
+import UIKit
+
+class CustomBtn1: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.backgroundColor = UIColor(red: 111/255, green: 160/255, blue: 250/255, alpha: 1)
+    }
+
+}
